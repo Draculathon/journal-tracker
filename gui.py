181519fb9432c7fc2be62c754cf63101d58154f8
Draculathon <elhorrimouad202@gmail.com>
2025-06-26 12:23:41 +0300

    Add filter by date GUI

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import tkinter
 import os
 from datetime import date
 from tkinter import messagebox
+import tkinter.simpledialog
 
 # Making sure 'journals' folder exists
 if not os.path.exists("journals"):
@@ -40,6 +41,8 @@ else:
 
                 for num, entry in enumerate(entries, start=1):
                     text.insert(tkinter.END, f"{num}. {entry}")
+                
+                # make the text box interactable
                 text.config(state="disabled")
                 
                 # create a scrollbar
@@ -52,10 +55,41 @@ else:
         except FileNotFoundError:
             messagebox.showerror("Error", "No entries found. Try adding some first.")
 
+    def filter_by_date():
+        try:
+            with open("journals/entries.txt", "r") as file:
+                entries = file.readlines()
+                
+                # Asking for required Date
+                user_date = tkinter.simpledialog.askstring("Date filter", "Enter the date to want to check YYYY-MM-DD :")
+                
+                # new window
+                top = tkinter.Toplevel(window)
+                top.title("Date Filter")
+                
+                # Add text box
+                text = tkinter.Text(top, height=10, width=50)
+                text.pack()
+
+                matching = []
+                for entry in entries:
+                    journal_date, journal = entry.strip().split("~")
+                    if user_date.strip() == journal_date.strip():
+                        matching.append(journal.strip())
+                
+                if matching:
+                    for item in matching:
+                        text.insert(tkinter.END, f"{item}\n")
+                else:
+                    text.insert(tkinter.END, "No entries found for that date.")
+                text.config(state="disabled")
+        except FileNotFoundError:
+            messagebox.showerror("Error", "No entries found. Try adding some first.")
+
     # Gui Setup
     window = tkinter.Tk()
     window.title("Journal Tracker")
-    window.geometry("400x300")
+    window.geometry("400x500")
 
     # Label
     label = tkinter.Label(window, text="Write your journal entry below:")
@@ -67,11 +101,15 @@ else:
 
     # Save Button     
     save_button = tkinter.Button(window, text="Save Entry", command=save_entry)
-    save_button.pack(pady=10)
+    save_button.pack(pady=4)
 
     # View entries Button
     view_button = tkinter.Button(window, text= "View entries", command=view_entries)
     view_button.pack(pady=4)
+
+    # Filter by Date Button
+    filter_button = tkinter.Button(window, text= "Filter by Date", command=filter_by_date)
+    filter_button.pack(pady= 4)
 
     # Run the app
     window.mainloop()
